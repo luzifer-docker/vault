7d2e44c706064ca0d00904c372f8e4ea3465b5bf
Knut Ahlers <knut@ahlers.me>
2021-12-02 02:05:05 +0100

    Update catalog api url
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- update-version.sh
+++ update-version.sh
@@ -12,7 +12,7 @@ git reset --hard origin/master
 ### ---- ###
 
 echo "Fetching latest version..."
-LATEST=$(curl -sSLf 'https://latest.luzifer.io/catalog-api/vault/latest.txt?p=version')
+LATEST=$(curl -sSLf 'https://lv.luzifer.io/v1/catalog/vault/latest/version')
 
 echo "Getting SHA256SUM for Linux AMD64 version..."
 SHASUM=$(curl -s https://releases.hashicorp.com/vault/${LATEST}/vault_${LATEST}_SHA256SUMS | awk '/linux_amd64/{print $1}')
@@ -33,7 +33,7 @@ docker build .
 echo "Updating repository..."
 git add Dockerfile
 git -c user.name='Travis Automated Update' -c user.email='travis@luzifer.io' \
-  commit -m "Vault ${LATEST}"
+	commit -m "Vault ${LATEST}"
 git tag ${LATEST}
 
 git push -q https://${GH_USER}:${GH_TOKEN}@github.com/luzifer-docker/vault.git master --tags
